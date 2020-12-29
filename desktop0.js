c573385c99fb791c138ea0f67f266500b84e52f9
LukeNK <64694123+LukeNk@users.noreply.github.com>
2020-12-29 10:48:36 +0700

    prepare everything lol

--- desktop0.js
+++ desktop0.js
@@ -1,2 +1,2 @@
 // The client to display for the host
-// This file is optional if you want to access the OS when you have web browser
+// This file is optional if you want to access the OS when you don't have have web browser
--- server.js
+++ server.js
@@ -1,1 +1,76 @@
 // The core which send data to clients (display)
+const express = require('express')
+const app = express();
+const fs = require('fs');
+const path = require('path');
+
+let SYSCONFIG; // Will init at readVar1();
+
+function debugLog(lv, location, message) {
+    let res;
+    if (location != '') {
+        res = `[${lv}@${location}] ${message}\n`;
+    } else {
+        res = `[${lv}] ${message}\n`;
+    }
+
+    function fileLog() {
+        if (lv == 'CRIT' || lv == 'MESS') { console.log(res) } else if (lv == 'STOP') { process.kill(process.pid) }
+        fs.appendFile('./var/session/log', res, function(err) {
+            if (err) { debugLog('CRIT', 'node.js', err) }
+        });
+
+    }
+    fileLog();
+}
+
+// Start Up
+fs.readdir('./var/session', (err, files) => {
+    if (err) { debugLog(`STOP`, 'node.js', 'Cannot read ./var/session\n------' + err); }
+
+    for (const file of files) {
+        fs.unlink(path.join('./var/session', file), err => {
+            if (err) { debugLog(`STOP`, 'node.js', 'Cannot clean ./var/session\n------' + err); };
+        });
+    }
+    debugLog(`MESS`, '', 'Waking up Velvety');
+    readVar1();
+});
+
+function readVar1() {
+    let tempID = Math.floor(Math.random() * 9999) + 1;
+    tempID = tempID.toString();
+
+    fs.writeFile('./var/session/ID', tempID, function(err) {
+        if (err) { debugLog('CRIT', 'server.readVar1', 'Cannot create session ID: ' + tempID + `\n------ ${err}`) }
+        debugLog('INFO', 'server.readVar1', `Session ID generated: ${tempID}`)
+    })
+    try {
+        SYSCONFIG = require('./var/sys.config')
+    } catch (err) {
+        debugLog('CRIT', 'node.js', err)
+        debugLog('CRIT', 'server.readVar1', 'Cannot find system config file, regenerate default setting');
+        const recover = require('./lib/sys.recover/sys.recover')
+        recover.configRecovery('./var/sys.config');
+    }
+
+    startUpCompleted();
+}
+
+function startUpCompleted() {
+    app.get('/', (req, res) => {
+        let rootWindows = SYSCONFIG.display.env.root;
+        if (rootWindows == '') { rootWindows = SYSCONFIG.console.app };
+        fs.readFile(rootWindows, function(err, data) {
+            res.send(data);
+        })
+    })
+
+    app.listen(3000, () => {
+        debugLog('STAT', '', `Port opened`)
+    })
+}
+
+function shutdown() {
+
+}
