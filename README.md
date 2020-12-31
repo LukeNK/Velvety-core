67f3f200bc8ea9e37f783a12e1a225d8456961ee
LukeNK <64694123+LukeNk@users.noreply.github.com>
2020-12-31 15:08:13 +0700

    Major update in system
    Async fs, captain console polished, prepare librarian package manager, added system modules, sys.config.json extended.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Velvety-core
- 
+A simple, HTML-Nodejs based operating system
--- server.js
+++ server.js
@@ -5,21 +5,28 @@ const fs = require('fs');
 const path = require('path');
 
 let SYSCONFIG; // Will init at readVar1();
+let SYSMODULES; // Will init at readVar2();
 
 function debugLog(lv, location, message) {
     let res;
     if (location != '') {
-        res = `[${lv}@${location}] ${message}\n`;
+        res = `[${lv}@${location}] ${message}`;
     } else {
-        res = `[${lv}] ${message}\n`;
+        res = `[${lv}] ${message}`;
     }
 
     function fileLog() {
         if (lv == 'CRIT' || lv == 'MESS') { console.log(res) } else if (lv == 'STOP') { process.kill(process.pid) }
-        fs.appendFile('./var/session/log', res, function(err) {
+        fs.appendFile('./var/session/log', res + '\n', function(err) {
             if (err) { debugLog('CRIT', 'node.js', err) }
         });
-
+        if (location) {
+            location = location.replace(/ /g, "");
+            location = location.replace(/\//g, "");
+            fs.appendFile(`./var/session/${location}-log`, res + '\n', function(err) {
+                if (err) { debugLog('CRIT', 'node.js', err) }
+            });
+        }
     }
     fileLog();
 }
@@ -53,20 +60,88 @@ function readVar1() {
         const recover = require('./lib/sys.recover/sys.recover')
         recover.configRecovery('./var/sys.config');
     }
+    readVar2();
+}
 
+function readVar2() {
+    // require modules for system to work
+    SYSMODULES = require('./lib/sys.modules/sys.modules');
     startUpCompleted();
 }
 
 function startUpCompleted() {
+    debugLog('MESS', '', 'Welcome to Velvety')
+
     app.get('/', (req, res) => {
-        let rootWindows = SYSCONFIG.display.env.root;
+        let rootWindows;
+        rootWindows = SYSCONFIG.display.root;
         if (rootWindows == '') { rootWindows = SYSCONFIG.console.app };
-        fs.readFile(rootWindows, function(err, data) {
+        fs.readFile(rootWindows, "utf8", function(err, data) {
+            if (err) { res.send('Cannot load basic root windows because\n' + err) }
+            res.set({
+                'Content-Type': 'text/html',
+            })
             res.send(data);
         })
     })
 
-    app.listen(3000, () => {
+    app.get('/lib/:name', function(req, res) {
+        let name = req.params.name;
+        let startupPath; // The excutable location
+        if (name.includes('.js')) {
+            startupPath = `lib/${name.slice(0, name.length-3)}/${name}`
+        } else if (name.includes('.html')) {
+            startupPath = `lib/${name.slice(0, name.length-5)}/${name}`
+        } else {
+            let metaData = require(`./lib/${name}/package`)
+            let startupData = metaData.main;
+            startupPath = `lib/${name}/${startupData}`;
+        }
+
+        fs.readFile(startupPath, "utf8", function(err, data) {
+            if (err) {
+                debugLog('CRIT', 'GET /lib', err);
+                res.send(err)
+            } else {
+                res.send(data);
+                debugLog('INFO', 'GET /lib', `${startupPath} Served`)
+            }
+        })
+    })
+
+    app.get('/fs', function(req, res) {
+        let type = req.query.type;
+        let authID = req.query.id;
+        let authPass = req.query.pass;
+        let path = req.query.path;
+
+        if (authID != SYSCONFIG.auth.ID || authPass != SYSCONFIG.auth.pass) {
+            res.send('err');
+            debugLog('INFO', 'GET /fs', 'Authentication failed')
+        } else {
+            debugLog('INFO', 'GET /fs', 'Authentication successfully')
+            res.send('ok')
+        }
+    })
+
+    app.post('/fs', function(req, res) {
+        let path = req.body.path;
+        let data = req.body.data;
+        let authID = req.query.id;
+        let authPass = req.query.pass;
+
+        if (authID != SYSCONFIG.auth.ID || authPass != SYSCONFIG.auth.pass) {
+            res.send('err');
+        } // if auth failed
+
+        if (type == 'desktop') {
+            if (data = 'new') {
+
+            }
+        }
+    })
+
+    app.listen(SYSCONFIG.serverjs.port, () => {
         debugLog('STAT', '', `Port opened`)
     })
 }
